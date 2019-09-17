0904dbdadafdb594dfca93db8a4b77b72fbdae73
Amy <ampyaephyonaing@gmail.com>
2019-09-17 15:43:38 +0800

    #UserRegister

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,18 +10,30 @@ namespace App\Http\Controllers\Auth;
 
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function register(Request $request){
 
         $this->validate($request,[
-            'email'=>'required',
+            'email'=>'required|email|max:50|unique:users',
             'password' => 'required',
-            'full_name' => 'required'
+            'full_name' => 'required|max:255'
         ]);
 
-        return 'Success Validation';
+        try{
+
+            $request['password'] =   Hash::make($request->password);
+
+            $user = User::create($request->all());
+
+            return response()->json(['status'=>'success','message'=>'Success','data'=>$user],201);
+
+        }catch (\Exception $e){
+            return response()->json(['status'=>'failed','message'=>'Internal Server Error','data'=>[]],500);
+        }
     }
 }
